53d2662f0b463291ba1dab1f05e6441467b923b1
Eason Lin <easonlin404@gmail.com>
2018-10-09 15:10:17 +0800

    add map-pointer-value example

---
+++ map-pointer-value/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type student struct {
+	Name string
+	Age  int
+}
+
+func pase_student() map[string]*student {
+	m := make(map[string]*student)
+	stus := []student{
+		{Name: "zhou", Age: 24},
+		{Name: "li", Age: 23},
+		{Name: "wang", Age: 22},
+	}
+	for _, stu := range stus {
+		m[stu.Name] = &stu
+	}
+	return m
+}
+func main() {
+	students := pase_student()
+
+	/*
+		Will print value all the same becaue all pointer to the last one stu.
+		key=li,value=&{wang 22}
+		key=wang,value=&{wang 22}
+		key=zhou,value=&{wang 22}
+	*/
+	for k, v := range students {
+		fmt.Printf("key=%s,value=%v \n", k, v)
+	}
+}
