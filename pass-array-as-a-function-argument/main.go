007d3277c1c804eb1daff4ea18966e504fae3b6a
Eason Lin <easonlin404@gmail.com>
2018-12-18 15:57:52 +0800

    feat; add pass array as function argument

---
+++ pass-array-as-a-function-argument/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := [3]int{1, 2, 3}
+
+	// passed by value
+	func(arr [3]int) {
+		arr[0] = 7
+		fmt.Println(arr) //prints [7 2 3]
+	}(x)
+	fmt.Println(x) //prints [1 2 3] (not ok if you need [7 2 3])
+
+	func(arr *[3]int) {
+		(*arr)[0] = 7
+		fmt.Println(arr) //prints &[7 2 3]
+	}(&x)
+	fmt.Println(x) //prints [7 2 3]
+
+}
